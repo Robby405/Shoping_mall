12148c305743e5723a5a8f894504e5d1069f68ae
zqw <1649280086@qq.com>
2018-05-27 14:15:56 +0800

    编程式路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,29 @@ import HelloWorld from '@/components/HelloWorld'
 import Test from '@/view/test'
 import Title1 from '@/view/Title1'
 import Title2 from '@/view/Title2'
+import Goods from '@/view/goods'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/test',
       name: 'HelloWorld',
       component: Test,
       children: [{
-      	path:'title1',
-      	name:'title1',
+      	path: 'title1',
+      	name: 'title1',
       	component: Title1
       },
       {
-      	path:'title2',
-      	name:'title2',
+      	path: 'title2',
+      	name: 'title2',
       	component: Title2
       }]
+    },
+    {
+      path: '/goods',
+      name: 'goods',
+      component: Goods
     }]
 })
