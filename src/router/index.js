c1d87b2659145cb3a9cd82f95da52bd2376376ae
zqw <1649280086@qq.com>
2018-05-28 11:55:36 +0800

    命名视图

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,41 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/view/test'
-import Title1 from '@/view/Title1'
-import Title2 from '@/view/Title2'
+import Title1 from '@/view/title1'
+import Title2 from '@/view/title2'
 import Goods from '@/view/goods'
 
 Vue.use(Router)
 
 export default new Router({
+  // routes: [{
+  //     path: '/test',
+  //     name: 'HelloWorld',
+  //     component: Test,
+  //     children: [{
+  //     	path: 'title1',
+  //     	name: 'title1',
+  //     	component: Title1
+  //     },
+  //     {
+  //     	path: 'title2',
+  //     	name: 'title2',
+  //     	component: Title2
+  //     }]
+  //   },
+  //   {
+  //     path: '/goods',
+  //     name: 'goods',
+  //     component: Goods
+  //   }]
+
   routes: [{
-      path: '/test',
-      name: 'HelloWorld',
-      component: Test,
-      children: [{
-      	path: 'title1',
-      	name: 'title1',
-      	component: Title1
-      },
-      {
-      	path: 'title2',
-      	name: 'title2',
-      	component: Title2
-      }]
-    },
-    {
-      path: '/goods',
-      name: 'goods',
-      component: Goods
-    }]
+    path: '/',
+    name: '/',
+    components: {
+      default: Goods,
+      title1: Title1,
+      title2: Title2
+    }
+  }]
 })
